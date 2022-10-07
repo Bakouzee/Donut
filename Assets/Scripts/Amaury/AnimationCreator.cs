d5b8de58555b1cac2fbef0e020039f251db01cc3
AngelSushi <angelsushigame@gmail.com>
2022-10-07 16:21:35 +0200

    Add AnimCreator Tool

---
+++ Assets/Scripts/Amaury/AnimationCreator.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCreator : MonoBehaviour
+{
+
+    public string animName;
+    public string animPath;
+    public Texture2D texture;
+
+    public RuntimeAnimatorController animator;
+    public bool isLooping;
+    public int timeBetweenSprite;
+
+}
