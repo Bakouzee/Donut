0eff319648e17fbe476f9fcf4ee318cf70703990
Amaury <amaury.mehlman@gmail.com>
2022-10-18 16:38:41 +0200

    Setup Maxi Map

--- Assets/Scripts/Amaury/MinimapController.cs
+++ Assets/Scripts/Amaury/MinimapController.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MinimapController : MonoBehaviour {
+    public bool isInMap;
 
-    public RenderTexture renderCamera;
+    public static MinimapController instance { get; private set; }
 
+    public GameObject minimap;
+    public GameObject map;
+    
+    void Awake() {
+        instance = this;
+    }
 
-    void Update() {
-        
+    public void OnMinimapManage(InputAction.CallbackContext e) {
+        if (e.performed) {
+            isInMap = !isInMap;
+            
+            map.SetActive(isInMap);
+            minimap.SetActive(!isInMap);
+        }
     }
 }
