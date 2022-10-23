63c8426c5e9fc4268b73ac4285586636a53f3906
Freddy <freddy_maupou@orange.fr>
2022-10-23 18:37:45 +0200

    [WIP] Enemies tools

--- Assets/Scripts/Freddy/Tools/DebugMenuBattlePhase.cs
+++ Assets/Scripts/Freddy/Tools/DebugMenuBattlePhase.cs
@@ -3,12 +3,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Animations;
 using UnityEngine;
 
-[CustomEditor(typeof(DebugMenuBattlePhase))]
+//[CustomEditor(typeof(DebugMenuBattlePhase))]
 public class DebugMenuBattlePhase : EditorWindow
 { 
     private static int numberEnemiesCreated = 0;
+    private static List<AllFighters> enemies = new List<AllFighters>();
+    private static List<bool> enemiesToDelete = new List<bool>();
+    private Vector2 scrollPos = Vector2.zero;
 
     [MenuItem("Tools/Debug Menu/Battle Phase")]
     static void InitDebugMenu()
@@ -16,25 +20,77 @@ public class DebugMenuBattlePhase : EditorWindow
         DebugMenuBattlePhase window = GetWindow<DebugMenuBattlePhase>();
         window.titleContent = new GUIContent("Debug Menu : Battle Phase");
         window.Show();
-       // this.myObject = (DebugMenuBattlePhase)this.target;
+        if(enemies.Count > 0)
+        {
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                enemies[i].Name = EditorGUILayout.TextField("Name", enemies[i].Name);
+                enemies[i].Level = EditorGUILayout.TextField("Level", enemies[i].Level);
+                enemies[i].Sprite = EditorGUILayout.ObjectField("Sprite", enemies[i].Sprite, typeof(Sprite), true) as Sprite;
+                enemies[i].AnimatorController = EditorGUILayout.ObjectField("Animator Controller", enemies[i].AnimatorController, typeof(AnimatorController), true) as AnimatorController;
+                enemies[i].TotalHealth = EditorGUILayout.IntField("Total Health", enemies[i].TotalHealth);
+                enemies[i].CurrentHealth = EditorGUILayout.IntField("Current Health", enemies[i].CurrentHealth);
+                enemies[i].MinDamage = EditorGUILayout.IntField("Min Damage", enemies[i].MinDamage);
+                enemies[i].MaxDamage = EditorGUILayout.IntField("Max Damage", enemies[i].MaxDamage);
+                enemies[i].Power = EditorGUILayout.IntField("Power", enemies[i].Power);
+                enemies[i].Healing = EditorGUILayout.IntField("Healing", enemies[i].Healing);
+                enemiesToDelete.Add(false);
+                enemiesToDelete[i] = EditorGUILayout.Toggle("Delete ?", enemiesToDelete[i]);
+            }
+        }
     }
 
     private void OnGUI()
-    { 
-        bool save = GUILayout.Button(new GUIContent("SAVE"));
-        bool add = GUILayout.Button(new GUIContent("ADD"));
+    {
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+
+        bool add = GUILayout.Button(new GUIContent("ADD")); 
 
         if (add)
         {
-            AllFighters newEnemy = CreateInstance<AllFighters>();
-            newFighter.Name = GUILayout.TextField(newFighter.Name);
-            newEnemy.fighters.Add() = 
-            AssetDatabase.CreateAsset(newEnemy, "Assets/Enemies/Enemy" + ++numberEnemiesCreated + ".asset");
+            enemies.Add(new AllFighters());
         }
+
+        for(int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].Name = EditorGUILayout.TextField("Name", enemies[i].Name);
+            enemies[i].Level = EditorGUILayout.TextField("Level", enemies[i].Level);
+            enemies[i].Sprite = EditorGUILayout.ObjectField("Sprite", enemies[i].Sprite, typeof(Sprite), true) as Sprite;
+            enemies[i].AnimatorController = EditorGUILayout.ObjectField("Animator Controller", enemies[i].AnimatorController, typeof(AnimatorController), true) as AnimatorController;
+            enemies[i].TotalHealth = EditorGUILayout.IntField("Total Health", enemies[i].TotalHealth);
+            enemies[i].CurrentHealth = EditorGUILayout.IntField("Current Health", enemies[i].CurrentHealth);
+            enemies[i].MinDamage = EditorGUILayout.IntField("Min Damage", enemies[i].MinDamage);
+            enemies[i].MaxDamage = EditorGUILayout.IntField("Max Damage", enemies[i].MaxDamage);
+            enemies[i].Power = EditorGUILayout.IntField("Power", enemies[i].Power);
+            enemies[i].Healing = EditorGUILayout.IntField("Healing", enemies[i].Healing);
+            enemiesToDelete.Add(false);
+            enemiesToDelete[i] = EditorGUILayout.Toggle("Delete ?", enemiesToDelete[i]);
+        }
+
+        bool save = GUILayout.Button(new GUIContent("SAVE"));
+        bool clear = GUILayout.Button(new GUIContent("CLEAR"));
 
         if (save)
         {
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                AssetDatabase.CreateAsset(enemies[i], "Assets/Enemies/Enemy" + ++numberEnemiesCreated + ".asset");
+            }
             AssetDatabase.SaveAssets();
         }
+
+        if (clear)
+        {
+            for (int i = 0; i < enemiesToDelete.Count; i++)
+            {
+                if (enemiesToDelete[i] == true)
+                {
+                    AssetDatabase.DeleteAsset("Assets/Enemies/Enemy" + (i + 1) + ".asset");
+                    enemiesToDelete.RemoveAt(i);
+                    enemies.RemoveAt(i);
+                }
+            }
+        }
+        EditorGUILayout.EndScrollView();
     }
 }
--- Assets/Scripts/Julien/UI/AllFighters.cs
+++ Assets/Scripts/Julien/UI/AllFighters.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 namespace Com.Donut.BattleSystem
@@ -7,6 +9,54 @@ namespace Com.Donut.BattleSystem
     [CreateAssetMenu]
     public class AllFighters : ScriptableObject
     {
-        public List<Fighter> fighters = new List<Fighter>();
+        //public List<Fighter> fighters = new List<Fighter>();
+        [SerializeField] private string _name;
+        [SerializeField] private string _level;
+        [SerializeField] private Sprite _sprite;
+        [SerializeField] private AnimatorController _animatorController;
+        [SerializeField] private int _currentHealth;
+        [SerializeField] private int _totalHealth;
+        [SerializeField] private int _minDamage;
+        [SerializeField] private int _maxDamage;
+        [SerializeField] private int _power;
+        [SerializeField] private int _healing;
+        private bool _isDead;
+
+        [SerializeField] private List<Abilities> abilities = new List<Abilities>();
+
+        public string Name { get { return _name;} set { _name = value;} }
+
+        public string Level { get { return _level; } set { _level = value; } }
+        public Sprite Sprite { get { return _sprite; } set { _sprite = value; } }
+        public AnimatorController AnimatorController { get { return _animatorController; } set { _animatorController = value; } }
+        public int TotalHealth { get { return _totalHealth; } set { _totalHealth = value; } }
+        public int CurrentHealth { get { return _currentHealth; } set { _currentHealth = value; } }
+        public int MinDamage { get { return _minDamage; } set { _minDamage = value; } }
+        public int MaxDamage { get { return _maxDamage; } set { _maxDamage = value; } }
+        public int Power { get { return _power; } set { _power = value; } }
+        public int Healing { get { return _healing; } set { _healing = value; } }
+        public bool IsDead { get { return _isDead; } set { _isDead = value; } }
+
+
+        public List<Abilities> Abilities { get { return abilities; } set { abilities = value; } }
+
+        public void Damage(int amount)
+        {
+            _currentHealth = Math.Max(0, _currentHealth - amount);
+            if (_currentHealth == 0)
+            {
+                _isDead = true;
+            }
+        }
+
+        public void Heal(int amount)
+        {
+            _currentHealth += amount;
+        }
+
+        private void OnValidate()
+        {
+            _currentHealth = Math.Min(_currentHealth, _totalHealth);
+        }
     }
 }
