6ccf6a003da622bd01a34a48c0527e6007a18c0e
Freddy <freddy_maupou@orange.fr>
2022-11-23 23:45:07 +0100

    [ADD] Forgot load player Health

--- Assets/Scripts/Julien/UI/Fighter.cs
+++ Assets/Scripts/Julien/UI/Fighter.cs
@@ -30,7 +30,7 @@ namespace Com.Donut.BattleSystem
         public Sprite Sprite => _sprite;
         public RuntimeAnimatorController AnimatorController => _animatorController;
         public int TotalHealth => _totalHealth;
-        public int CurrentHealth => _currentHealth;
+        public int CurrentHealth { get { return _currentHealth; } set { _currentHealth = value; } }
         public int Power => _power;
         public int Healing => _healing;
         public bool IsDead => _isDead;
